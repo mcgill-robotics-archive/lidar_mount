1668af23e3c3ce9948690d43ecd1fe95a3f244ff
Malcolm Watt <malcolm.watt@mail.mcgill.ca>
2017-01-20 16:19:29 -0500

    Modify scan assembler for testing.

--- scripts/request_pointcloud.py
+++ scripts/request_pointcloud.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
-import roslib; roslib.load_manifest('laser_assembler')
-import rospy; from laser_assembler.srv import *
+import rospy
+from laser_assembler.srv import *
+from sensor_msgs.msg import PointCloud
 
-rospy.init_node("test_client")
-rospy.wait_for_service("assemble_scans")
-try:
-    assemble_scans = rospy.ServiceProxy('assemble_scans', AssembleScans)
-    resp = assemble_scans(rospy.Time(0,0), rospy.get_rostime())
-    print "Got cloud with %u points" % len(resp.cloud.points)
-except rospy.ServiceException, e:
-    print "Service call failed: %s"%e
+if __name__ == '__main__':
+    rospy.init_node("test_client")
+    rospy.wait_for_service("assemble_scans")
+
+    while True:
+        r = rospy.Rate(20)
+        pub = rospy.Publisher('lidar_pc_pub', PointCloud, queue_size=5)
+        try:
+            assemble_scans = rospy.ServiceProxy('assemble_scans', AssembleScans)
+            resp = assemble_scans(rospy.Time(0,0), rospy.get_rostime())
+            pub.publish(resp.cloud)
+        except KeyboardInterrupt, er:
+            break
