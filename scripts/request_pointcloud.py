d7a035151aa8d2203548e3831aecc5be4e66ac09
Malcolm Watt <malcolm.watt@mail.mcgill.ca>
2017-03-10 22:30:21 -0500

    Fix name of topic we publish pointclouds to.

--- scripts/request_pointcloud.py
+++ scripts/request_pointcloud.py
@@ -11,12 +11,16 @@ if __name__ == '__main__':
     rospy.wait_for_service("assemble_scans")
 
     while True:
-        r = rospy.Rate(1)
-        pub = rospy.Publisher('lidar_pc_pub', PointCloud, queue_size=5)
+        r = rospy.Rate(1)  # Publishing rate for the point clouds.
+
+        pub = rospy.Publisher('lidar_mount/pointclouds',
+                              PointCloud, queue_size=5)
         try:
+            # Request PointCloud from laser_assembler and publish.
             scanner_srv = rospy.ServiceProxy('assemble_scans', AssembleScans)
             resp = scanner_srv(rospy.Time(0, 0), rospy.get_rostime())
             pub.publish(resp.cloud)
         except KeyboardInterrupt as er:
             break
+
         r.sleep()
